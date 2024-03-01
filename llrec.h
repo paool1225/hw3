81694637928355f13fcea0e6afa6f3218c50fca0
Paul Gomez <psgomez@usc.edu>
2024-03-01 00:24:25 +0000

    another commit for HW3

--- llrec.h
+++ llrec.h
@@ -14,24 +14,9 @@ struct Node
     Node(int v, Node* n) : val(v), next(n) {}
 };
 
-/**
- * Divides the list into two lists based on the pivot value.
- * All nodes with values less than the pivot will be moved to the 'smaller' list,
- * and all nodes with values greater or equal to the pivot will be moved to the 'larger' list.
- * @param head Reference to the original list's head.
- * @param smaller Reference to the head of the resulting 'smaller' list.
- * @param larger Reference to the head of the resulting 'larger' list.
- * @param pivot The pivot value for division.
- */
 
 void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot);
-/**
- * Filters elements of the linked list based on a predicate and returns a new list.
- * The original list is not modified.
- * @param head Pointer to the head of the list to be filtered.
- * @param pred Predicate function that returns true for elements to be included.
- * @return Pointer to the head of the filtered list.
- */
+
 template <typename Comp>
 Node* llfilter(Node* head, Comp pred);
 
