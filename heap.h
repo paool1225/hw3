a0046af1908c7320737ea6f514d5312d44d0dfa0
Paul Gomez <psgomez@usc.edu>
2024-03-01 09:06:44 +0000

    yet again again another commit for HW3

--- heap.h
+++ heap.h
@@ -1,115 +1,106 @@
 #ifndef HEAP_H
 #define HEAP_H
+
 #include <functional>
 #include <stdexcept>
 #include <vector>
 
-
-template <typename T, typename PComparator = std::less<T> >
+template <typename T, typename PComparator = std::less<T>>
 class Heap
 {
 public:
-
-  Heap(int m=2, PComparator c = PComparator());
-  ~Heap();
-  void push(const T& item);
-  T const & top() const;
-  void pop();
-  bool empty() const;
-  int size() const;
+    Heap(int m=2, PComparator c = PComparator());
+    ~Heap() = default; // Use default destructor
+    void push(const T& item);
+    T const & top() const;
+    void pop();
+    bool empty() const;
+    int size() const;
 
 private:
-  void heapify(size_t index);
-  PComparator comp;
-  int k_ary;
-  std::vector<T> h;
+    std::vector<T> data;
+    PComparator comp;
+    int m; // Arity of the heap
 
+    void heapifyUp(size_t index);
+    void heapifyDown(size_t index);
 };
+
 template <typename T, typename PComparator>
-bool Heap<T,PComparator>::empty() const{
-  return h.size()==0;
+Heap<T, PComparator>::Heap(int m, PComparator c) : comp(c), m(m) {
+    if (m < 2) {
+        throw std::invalid_argument("Heap arity must be at least 2");
+    }
 }
 
 template <typename T, typename PComparator>
-Heap<T,PComparator>::Heap(int m, PComparator c){
-  comp = c;
-  if(m < 2){
-    throw std::logic_error("can't have value under 2");
-  }
-  k_ary = m;
-
-}
-template <typename T, typename PComparator>
-Heap<T,PComparator>::~Heap(){
-  
+void Heap<T, PComparator>::push(const T& item) {
+    data.push_back(item);
+    heapifyUp(data.size() - 1);
 }
 
 template <typename T, typename PComparator>
-int Heap<T,PComparator>::size() const{
-  return h.size();
+T const & Heap<T, PComparator>::top() const {
+    if (empty()) {
+        throw std::underflow_error("Heap is empty");
+    }
+    return data.front();
 }
 
 template <typename T, typename PComparator>
-void Heap<T,PComparator>::heapify(size_t index){
-  size_t leafid; 
-  size_t pri = k_ary*index + 1; 
+void Heap<T, PComparator>::pop() {
+    if (empty()) {
+        throw std::underflow_error("Heap is empty");
+    }
+    std::swap(data.front(), data.back());
+    data.pop_back();
+    if (!empty()) {
+        heapifyDown(0);
+    }
+}
 
-  if(k_ary*index + 1 > h.size()-1){
-    return;
-  }
-  else{
-    for(int i = 2; i <= k_ary; i++){
-      leafid = k_ary*index + i;
-      if(leafid < h.size()){
-        if(comp(h[leafid], h[pri])){
-          pri = leafid;
+template <typename T, typename PComparator>
+bool Heap<T, PComparator>::empty() const {
+    return data.empty();
+}
+
+template <typename T, typename PComparator>
+int Heap<T, PComparator>::size() const {
+    return data.size();
+}
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::heapifyUp(size_t index) {
+    while (index > 0) {
+        size_t parent = (index - 1) / m;
+        if (comp(data[index], data[parent])) {
+            std::swap(data[index], data[parent]);
+            index = parent;
+        } else {
+            break;
         }
-      }
     }
-    if(comp(h[pri], h[index])){
-      std::swap(h[pri], h[index]);
-      heapify(pri);
-    }
-  }
 }
 
 template <typename T, typename PComparator>
-void Heap<T,PComparator>::push(const T& item){
-    h.push_back(item);
-    int loc = h.size()-1;
-    int parent = (loc-1)/k_ary;
-    while(loc >= 0 && comp(h[loc], h[parent])){ 
-        std::swap(h[parent], h[loc]);
-        loc = parent;
-        parent = (loc-1)/k_ary;
+void Heap<T, PComparator>::heapifyDown(size_t index) {
+    size_t child = m * index + 1; // First child
+    while (child < data.size()) {
+        size_t minChild = child;
+        for (int i = 1; i < m; ++i) { // Find smallest child
+            size_t nextChild = m * index + 1 + i;
+            if (nextChild < data.size() && comp(data[nextChild], data[minChild])) {
+                minChild = nextChild;
+            }
+        }
+        if (comp(data[minChild], data[index])) {
+            std::swap(data[index], data[minChild]);
+            index = minChild;
+            child = m * index + 1;
+        } else {
+            break;
+        }
     }
 }
-template <typename T, typename PComparator>
-T const & Heap<T,PComparator>::top() const
-{
-  
-  if(empty()){
-    
-    throw std::underflow_error("Heap is empty");
-  }
- 
-  return h[0]; 
 
-}
-template <typename T, typename PComparator>
-void Heap<T,PComparator>::pop()
-{
-  if(empty()){
-    
-    throw std::underflow_error("Heap is empty");
-
-  }
-  if(h.size() == 1){
-    h.pop_back();
-    return;
-  }
-  std::swap(h[0], h[h.size()-1]);
-  h.pop_back();
-  heapify(0);
-}
-#endif
+#endif 
